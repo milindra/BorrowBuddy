83031b0de16cf14723f52dfc049c7a9e8ec4c358
milindra <milindrakanwar@gmail.com>
2017-03-22 19:23:49 +0530

    delete unnesarry files

--- a.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-echo "1";
-echo $_SESSION['email'];
-echo $_SESSION['pass'];
-echo "2";
-?>
--- abc.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-	$email=$_SESSION["email"];
-	echo "".$email;
-
-	echo "string";
-	
-?>
